2f716f803b09e6a2d22fc95f6d74be0f44d86e3c
bowtiemedical <tarannum.aiub@gmail.com>
2021-09-21 17:48:01 -0400

    added health__pc data needs replacing with Obeservation

--- force-app/main/default/lwc/healthBehaviorDisplay/healthBehaviorDisplay.js
+++ force-app/main/default/lwc/healthBehaviorDisplay/healthBehaviorDisplay.js
@@ -1,23 +1,54 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+const FIELDS = [
+    'Account.Name',
+    'Account.High_number_of_drinks__pc',
+    'Account.Cut_Down_Alcohol__pc',
+    'Account.Criticized_for_Drinking__pc',
+    'Account.Drink_in_Morning__pc',
+    'Account.Guilt_about_Drinking__pc',
+    'Account.Physical_Activity__pc'
+]
 
 export default class HealthBehaviorDisplay extends LightningElement {
 
-    @track
-    contacts = [{
-            Id: 1,
-            Name: 'Amy Taylor',
-            Title: 'VP of Engineering',
-        },
-        {
-            Id: 2,
-            Name: 'Michael Jones',
-            Title: 'VP of Sales',
-        },
-        {
-            Id: 3,
-            Name: 'Jennifer Wu',
-            Title: 'CEO',
-        },
-    ];
+    @api recordId;
+    error;
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS }) account;
 
+    get name() {
+            //console.log(this.account.data.fields.Name.value);
+            return this.account.data.fields.Name.value;
+
+        }
+        // Dringking Problem TODO: add logic
+
+    get highNumberOfDrinks() {
+        return this.account.data.fields.High_number_of_drinks__pc.value;
+    }
+
+    get alcohol() {
+        return this.account.data.fields.Cut_Down_Alcohol__pc.value;
+    }
+    get criticisedForAlcohol() {
+        return this.account.data.fields.Criticized_for_Drinking__pc.value;
+    }
+    get morningDringking() {
+        return this.account.data.fields.Drink_in_Morning__pc.value;
+    }
+
+    get guiltDrinking() {
+        return this.account.data.fields.Guilt_about_Drinking__pc.value;
+    }
+
+    // Healthy Habits
+
+    get physicalActivity() {
+
+        if (this.account.data.fields.Physical_Activity.value) {
+            str = "My physical activity is for " + this.account.data.fields.Physical_Activity__pc.value + " minutes"
+            return str
+        }
+
+    }
 }
