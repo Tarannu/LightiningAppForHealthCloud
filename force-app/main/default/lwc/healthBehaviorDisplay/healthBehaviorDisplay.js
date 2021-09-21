23b69ff713daacc3e74572b671abd59c4bdb711b
bowtiemedical <tarannum.aiub@gmail.com>
2021-09-21 12:30:45 -0400

    creating contact with lwc

--- force-app/main/default/lwc/healthBehaviorDisplay/healthBehaviorDisplay.js
+++ force-app/main/default/lwc/healthBehaviorDisplay/healthBehaviorDisplay.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HealthBehaviorDisplay extends LightningElement {}
+export default class HealthBehaviorDisplay extends LightningElement {
+
+    @track
+    contacts = [{
+            Id: 1,
+            Name: 'Amy Taylor',
+            Title: 'VP of Engineering',
+        },
+        {
+            Id: 2,
+            Name: 'Michael Jones',
+            Title: 'VP of Sales',
+        },
+        {
+            Id: 3,
+            Name: 'Jennifer Wu',
+            Title: 'CEO',
+        },
+    ];
+
+}
