e7941e4a7e612c800cc6d349a126f1bcab3eb786
bowtiemedical <tarannum.aiub@gmail.com>
2021-10-04 16:27:28 -0400

    added apex class but doesnt display Care obv

--- force-app/main/default/lwc/healthBehaviorDisplay/healthBehaviorDisplay.js
+++ force-app/main/default/lwc/healthBehaviorDisplay/healthBehaviorDisplay.js
@@ -1,15 +1,22 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
+import getCareObservation from '@salesforce/apex/HealthBehavior.relatedObservation';
 const FIELDS = [
     'Account.Name',
     'Account.Care_Plan_Recommendations__pc'
 ]
-
+const CRFIELDS = [
+    'CareObservation.Name',
+    'CareObservation.ObservedSubjectId'
+]
 export default class HealthBehaviorDisplay extends LightningElement {
     @api recordId;
 
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     account;
+
+    @wire(getCareObservation, { accountId: '$recordId', fields: CRFIELDS })
+    careObsList;
 
     get name() {
         return this.account.data.fields.Name.value;
@@ -17,4 +24,9 @@ export default class HealthBehaviorDisplay extends LightningElement {
     get carePlanRecommendations() {
         return this.account.data.fields.Care_Plan_Recommendations__pc.value;
     }
+    get careObservation() {
+        return this.careObsList.data.fields.Name.value;
+    }
+
+
 }
